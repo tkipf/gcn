c3293a9bad06dd13b5f6edbf4d35d2a959b60185
Yaguang <liyaguang0123@gmail.com>
2017-02-19 21:24:12 -0800

    Changes to named arguments. (Fix for tf 1.0.0)
    To avoid confusion, tf.nn.softmax_cross_entropy_with_logits only accepts named arguments in Tensorflow 1.0.0.

--- gcn/metrics.py
+++ gcn/metrics.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 
 def masked_softmax_cross_entropy(preds, labels, mask):
     """Softmax cross-entropy loss with masking."""
-    loss = tf.nn.softmax_cross_entropy_with_logits(preds, labels)
+    loss = tf.nn.softmax_cross_entropy_with_logits(logits=preds, lables=labels)
     mask = tf.cast(mask, dtype=tf.float32)
     mask /= tf.reduce_mean(mask)
     loss *= mask
